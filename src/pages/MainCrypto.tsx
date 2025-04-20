ac96f7e78a867357994df3a2ff9241c3bb1ad3e7
sean-waev <98407121+sean-waev@users.noreply.github.com>
2025-04-19 18:40:15 -0700

    license

--- src/pages/MainCrypto.tsx
+++ src/pages/MainCrypto.tsx
@@ -151,9 +151,9 @@ function MainCrypto() {
                   }}
                 >
                   The MIT License (MIT)
-                  <br />
+
                   Copyright (c) 2024 Y Combinator Hacker News
-                  {/* <br />
+
                   Permission is hereby granted, free of charge, to any person
                   obtaining a copy of this software and associated documentation
                   files (the "Software"), to deal in the Software without
@@ -162,11 +162,11 @@ function MainCrypto() {
                   sell copies of the Software, and to permit persons to whom the
                   Software is furnished to do so, subject to the following
                   conditions:
-                  <br />
+
                   The above copyright notice and this permission notice shall be
                   included in all copies or substantial portions of the
                   Software.
-                  <br />
+
                   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
                   KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
                   WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
@@ -174,7 +174,7 @@ function MainCrypto() {
                   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
                   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
                   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-                  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */}
+                  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
                   <br />
                 </center>
               </td>
