b7d4df4622730873a8da2ba69e0de327432cae65
sean-waev <98407121+sean-waev@users.noreply.github.com>
2025-05-19 15:06:42 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-// import "./App.css";
-// // import { usePrivy } from "@privy-io/react-auth";
-// import "../public/news.css"; // Adjust the path to your CSS file
+import "./App.css";
+// import { usePrivy } from "@privy-io/react-auth";
+import "../public/news.css"; // Adjust the path to your CSS file
 // import { Route, Routes } from "react-router-dom";
 // import MainCrypto from "./pages/MainCrypto";
 // import CommentsCrypto from "./pages/CommentsCrypto";
@@ -12,7 +12,7 @@
 // import CryptoSubmit from "./pages/cryptoSubmit";
 // import CryptoProfile from "./pages/cryptoProfile";
 // import CryptoUser from "./pages/cryptoUser";
-// // import CryptoNewsItem from "./pages/cryptoNewsItem";
+// import CryptoNewsItem from "./pages/cryptoNewsItem";
 // import CryptoCommentSelected from "./pages/cryptoCommentSelected";
 // import CryptoReplySelected from "./pages/cryptoReplySelected";
 // import CryptoJobSelected from "./pages/cryptoJobSelected";
@@ -74,177 +74,177 @@
 // import CryptoNewsItem from "./pages/cryptoNewsItem";
 // import AINewsItem from "./pages/aiNewsItem";
 
-// function App() {
-//   // const { login, logout, authenticated } = usePrivy();
+function App() {
+  // const { login, logout, authenticated } = usePrivy();
 
-//   // const clickSubmit = () => {
-//   //   // handle submit click
-//   // };
+  // const clickSubmit = () => {
+  //   // handle submit click
+  // };
 
-//   return (
-//     <Routes>
-//       <Route path="/" element={<TopTopNetwork />} />
-//       {/* crypto */}
-//       <Route path="/crypto" element={<MainCrypto />} />
-//       <Route path="/crypto-new" element={<CryptoNew />} />
-//       <Route path="/crypto-comments" element={<CommentsCrypto />} />
-//       <Route path="/crypto-ask" element={<CryptoAsk />} />
-//       <Route path="/crypto-show" element={<CryptoShow />} />
-//       <Route path="/crypto/jobs" element={<CryptoJobs />} />
-//       <Route path="/crypto-submit" element={<CryptoSubmit />} />
+  return (
+    <Routes>
+{/*       <Route path="/" element={<TopTopNetwork />} /> */}
+      {/* crypto */}
+{/*       <Route path="/crypto" element={<MainCrypto />} />
+      <Route path="/crypto-new" element={<CryptoNew />} />
+      <Route path="/crypto-comments" element={<CommentsCrypto />} />
+      <Route path="/crypto-ask" element={<CryptoAsk />} />
+      <Route path="/crypto-show" element={<CryptoShow />} />
+      <Route path="/crypto/jobs" element={<CryptoJobs />} />
+      <Route path="/crypto-submit" element={<CryptoSubmit />} />
 
-//       <Route path="/profile" element={<CryptoProfile />} />
-//       <Route path="/crypto-profile-jobs" element={<CryptoProfileJobs />} />
+      <Route path="/profile" element={<CryptoProfile />} />
+      <Route path="/crypto-profile-jobs" element={<CryptoProfileJobs />} />
 
-//       <Route
-//         path="/crypto/comments/selected/:id"
-//         element={<CryptoCommentSelected />}
-//       />
-//       <Route path="/crypto/comments/reply/:id" element={<CryptoReplySelected />} />
+      <Route
+        path="/crypto/comments/selected/:id"
+        element={<CryptoCommentSelected />}
+      />
+      <Route path="/crypto/comments/reply/:id" element={<CryptoReplySelected />} />
 
-//       <Route path="/crypto/item/:id" element={<CryptoNewsItem />} />
+      <Route path="/crypto/item/:id" element={<CryptoNewsItem />} />
 
-//       <Route path="/crypto/jobs-selected" element={<CryptoJobSelected />} />
+      <Route path="/crypto/jobs-selected" element={<CryptoJobSelected />} />
 
-//       <Route path="/user/:id" element={<CryptoUser />} />
-//       <Route path="/user-comments" element={<CryptoUserComments />} />
-//       <Route path="/user-submissions" element={<CryptoUserSubmissions />} />
-//       <Route path="/user-votes" element={<CryptoUserVotes />} />
+      <Route path="/user/:id" element={<CryptoUser />} />
+      <Route path="/user-comments" element={<CryptoUserComments />} />
+      <Route path="/user-submissions" element={<CryptoUserSubmissions />} />
+      <Route path="/user-votes" element={<CryptoUserVotes />} />
+ */}
+      {/* ai */}
+{/*       <Route path="/ai" element={<MainAI />} />
+      <Route path="/ai/new" element={<AINew />} />
+      <Route path="/ai/comments" element={<CommentsAI />} />
+      <Route path="/ai/ask" element={<AIAsk />} />
+      <Route path="/ai/show" element={<AIShow />} />
+      <Route path="/ai/jobs" element={<AIJobs />} />
+      <Route path="/ai/submit" element={<AISubmit />} />
 
-//       {/* ai */}
-//       <Route path="/ai" element={<MainAI />} />
-//       <Route path="/ai/new" element={<AINew />} />
-//       <Route path="/ai/comments" element={<CommentsAI />} />
-//       <Route path="/ai/ask" element={<AIAsk />} />
-//       <Route path="/ai/show" element={<AIShow />} />
-//       <Route path="/ai/jobs" element={<AIJobs />} />
-//       <Route path="/ai/submit" element={<AISubmit />} />
+      <Route path="/ai/comments/selected" element={<CommentsCrypto />} />
+      <Route path="/ai/comments/reply" element={<CommentsCrypto />} />
 
-//       <Route path="/ai/comments/selected" element={<CommentsCrypto />} />
-//       <Route path="/ai/comments/reply" element={<CommentsCrypto />} />
+      <Route path="/ai/item/:id" element={<AINewsItem />} />
 
-//       <Route path="/ai/item/:id" element={<AINewsItem />} />
+      <Route path="/ai/jobs/selected" element={<CommentsCrypto />} />
 
-//       <Route path="/ai/jobs/selected" element={<CommentsCrypto />} />
+   */}
+      {/* memecoins */}
+{/*       <Route path="/memecoins" element={<MainMemecoin />} />
+      <Route path="/memecoins/new" element={<MemecoinsNew />} />
+      <Route path="/memecoins/comments" element={<CommentsMemecoins />} />
+      <Route path="/memecoins/ask" element={<MemecoinsAsk />} />
+      <Route path="/memecoins/show" element={<MemecoinsShow />} />
+      <Route path="/memecoins/jobs" element={<MemecoinsJobs />} />
+      <Route path="/memecoins/submit" element={<MemecoinsSubmit />} />
 
-  
-//       {/* memecoins */}
-//       <Route path="/memecoins" element={<MainMemecoin />} />
-//       <Route path="/memecoins/new" element={<MemecoinsNew />} />
-//       <Route path="/memecoins/comments" element={<CommentsMemecoins />} />
-//       <Route path="/memecoins/ask" element={<MemecoinsAsk />} />
-//       <Route path="/memecoins/show" element={<MemecoinsShow />} />
-//       <Route path="/memecoins/jobs" element={<MemecoinsJobs />} />
-//       <Route path="/memecoins/submit" element={<MemecoinsSubmit />} />
+      <Route path="/memecoins/comments/selected" element={<CommentsCrypto />} />
+      <Route path="/memecoins/comments/reply" element={<CommentsCrypto />} />
 
-//       <Route path="/memecoins/comments/selected" element={<CommentsCrypto />} />
-//       <Route path="/memecoins/comments/reply" element={<CommentsCrypto />} />
+      <Route path="/memecoins/item" element={<CommentsCrypto />} />
 
-//       <Route path="/memecoins/item" element={<CommentsCrypto />} />
+      <Route path="/memecoins/jobs/selected" element={<CommentsCrypto />} />
 
-//       <Route path="/memecoins/jobs/selected" element={<CommentsCrypto />} />
+  */}
+      {/* dePIN */}
+{/*       <Route path="/depin" element={<MainDePIN />} />
+      <Route path="/depin/new" element={<DePINNew />} />
+      <Route path="/depin/comments" element={<CommentsDePIN />} />
+      <Route path="/depin/ask" element={<DePINAsk />} />
+      <Route path="/depin/show" element={<DePINShow />} />
+      <Route path="/depin/jobs" element={<DePINJobs />} />
+      <Route path="/depin/submit" element={<DePINSubmit />} />
 
- 
-//       {/* dePIN */}
-//       <Route path="/depin" element={<MainDePIN />} />
-//       <Route path="/depin/new" element={<DePINNew />} />
-//       <Route path="/depin/comments" element={<CommentsDePIN />} />
-//       <Route path="/depin/ask" element={<DePINAsk />} />
-//       <Route path="/depin/show" element={<DePINShow />} />
-//       <Route path="/depin/jobs" element={<DePINJobs />} />
-//       <Route path="/depin/submit" element={<DePINSubmit />} />
+      <Route path="/depin/comments/selected" element={<CommentsCrypto />} />
+      <Route path="/depin/comments/reply" element={<CommentsCrypto />} />
 
-//       <Route path="/depin/comments/selected" element={<CommentsCrypto />} />
-//       <Route path="/depin/comments/reply" element={<CommentsCrypto />} />
+      <Route path="/depin/item" element={<CommentsCrypto />} />
 
-//       <Route path="/depin/item" element={<CommentsCrypto />} />
+      <Route path="/depin/jobs/selected" element={<CommentsCrypto />} />
 
-//       <Route path="/depin/jobs/selected" element={<CommentsCrypto />} />
+ */}
 
+      {/* NFT */}
+{/*       <Route path="/nft" element={<MainNFT />} />
+      <Route path="/nft/new" element={<NFTNew />} />
+      <Route path="/nft/comments" element={<CommentsNFT />} />
+      <Route path="/nft/ask" element={<NFTAsk />} />
+      <Route path="/nft/show" element={<NFTShow />} />
+      <Route path="/nft/jobs" element={<NFTJobs />} />
+      <Route path="/nft/submit" element={<NFTSubmit />} />
 
+      <Route path="/nft/profile" element={<CommentsCrypto />} />
+      <Route path="/nft/profile/jobs" element={<CommentsCrypto />} />
 
-//       {/* NFT */}
-//       <Route path="/nft" element={<MainNFT />} />
-//       <Route path="/nft/new" element={<NFTNew />} />
-//       <Route path="/nft/comments" element={<CommentsNFT />} />
-//       <Route path="/nft/ask" element={<NFTAsk />} />
-//       <Route path="/nft/show" element={<NFTShow />} />
-//       <Route path="/nft/jobs" element={<NFTJobs />} />
-//       <Route path="/nft/submit" element={<NFTSubmit />} />
+      <Route path="/nft/comments/selected" element={<CommentsCrypto />} />
+      <Route path="/nft/comments/reply" element={<CommentsCrypto />} />
 
-//       <Route path="/nft/profile" element={<CommentsCrypto />} />
-//       <Route path="/nft/profile/jobs" element={<CommentsCrypto />} />
+      <Route path="/nft/item" element={<CommentsCrypto />} />
 
-//       <Route path="/nft/comments/selected" element={<CommentsCrypto />} />
-//       <Route path="/nft/comments/reply" element={<CommentsCrypto />} />
+      <Route path="/nft/jobs/selected" element={<CommentsCrypto />} />
 
-//       <Route path="/nft/item" element={<CommentsCrypto />} />
+      <Route path="/nft/user" element={<CommentsCrypto />} />
+      <Route path="/nft/user/comments" element={<CommentsCrypto />} />
+      <Route path="/nft/user/submissions" element={<CommentsCrypto />} />
+      <Route path="/nft/user/votes" element={<CryptoUserVotes />} />
+ */}
+      {/* DeSci */}
+{/*       <Route path="/desci" element={<MainDeSci />} />
+      <Route path="/desci/new" element={<DeSciNew />} />
+      <Route path="/desci/comments" element={<CommentsDeSci />} />
+      <Route path="/desci/ask" element={<DeSciAsk />} />
+      <Route path="/desci/show" element={<DeSciShow />} />
+      <Route path="/desci/jobs" element={<DeSciJobs />} />
+      <Route path="/desci/submit" element={<DeSciSubmit />} />
 
-//       <Route path="/nft/jobs/selected" element={<CommentsCrypto />} />
+      <Route path="/desci/comments/selected" element={<CommentsCrypto />} />
+      <Route path="/desci/comments/reply" element={<CommentsCrypto />} />
 
-//       <Route path="/nft/user" element={<CommentsCrypto />} />
-//       <Route path="/nft/user/comments" element={<CommentsCrypto />} />
-//       <Route path="/nft/user/submissions" element={<CommentsCrypto />} />
-//       <Route path="/nft/user/votes" element={<CryptoUserVotes />} />
+      <Route path="/desci/item" element={<CommentsCrypto />} />
 
-//       {/* DeSci */}
-//       <Route path="/desci" element={<MainDeSci />} />
-//       <Route path="/desci/new" element={<DeSciNew />} />
-//       <Route path="/desci/comments" element={<CommentsDeSci />} />
-//       <Route path="/desci/ask" element={<DeSciAsk />} />
-//       <Route path="/desci/show" element={<DeSciShow />} />
-//       <Route path="/desci/jobs" element={<DeSciJobs />} />
-//       <Route path="/desci/submit" element={<DeSciSubmit />} />
+      <Route path="/desci/jobs/selected" element={<CommentsCrypto />} />
+ */}
 
-//       <Route path="/desci/comments/selected" element={<CommentsCrypto />} />
-//       <Route path="/desci/comments/reply" element={<CommentsCrypto />} />
+      {/* film */}
+{/*       <Route path="/film" element={<MainFilm />} />
+      <Route path="/film/new" element={<FilmNew />} />
+      <Route path="/film/comments" element={<CommentsFilm />} />
+      <Route path="/film/ask" element={<FilmAsk />} />
+      <Route path="/film/show" element={<FilmShow />} />
+      <Route path="/film/jobs" element={<FilmJobs />} />
+      <Route path="/film/submit" element={<FilmSubmit />} />
 
-//       <Route path="/desci/item" element={<CommentsCrypto />} />
+      <Route path="/film/comments/selected" element={<CommentsCrypto />} />
+      <Route path="/film/comments/reply" element={<CommentsCrypto />} />
 
-//       <Route path="/desci/jobs/selected" element={<CommentsCrypto />} />
+      <Route path="/film/item" element={<CommentsCrypto />} />
 
+      <Route path="/film/jobs/selected" element={<CommentsCrypto />} />
+ */}
+      {/* gaming */}
+{/*       <Route path="/gaming" element={<MainGaming />} />
+      <Route path="/gaming/new" element={<GamingNew />} />
+      <Route path="/gaming/comments" element={<CommentsGaming />} />
+      <Route path="/gaming/ask" element={<GamingAsk />} />
+      <Route path="/gaming/show" element={<GamingShow />} />
+      <Route path="/gaming/jobs" element={<GamingJobs />} />
+      <Route path="/gaming/submit" element={<GamingSubmit />} />
 
-//       {/* film */}
-//       <Route path="/film" element={<MainFilm />} />
-//       <Route path="/film/new" element={<FilmNew />} />
-//       <Route path="/film/comments" element={<CommentsFilm />} />
-//       <Route path="/film/ask" element={<FilmAsk />} />
-//       <Route path="/film/show" element={<FilmShow />} />
-//       <Route path="/film/jobs" element={<FilmJobs />} />
-//       <Route path="/film/submit" element={<FilmSubmit />} />
+      <Route path="/gaming/profile" element={<CommentsCrypto />} />
+      <Route path="/gaming/profile/jobs" element={<CommentsCrypto />} />
 
-//       <Route path="/film/comments/selected" element={<CommentsCrypto />} />
-//       <Route path="/film/comments/reply" element={<CommentsCrypto />} />
+      <Route path="/gaming/comments/selected" element={<CommentsCrypto />} />
+      <Route path="/gaming/comments/reply" element={<CommentsCrypto />} />
 
-//       <Route path="/film/item" element={<CommentsCrypto />} />
+      <Route path="/gaming/item" element={<CommentsCrypto />} />
 
-//       <Route path="/film/jobs/selected" element={<CommentsCrypto />} />
+      <Route path="/gaming/jobs/selected" element={<CommentsCrypto />} />
 
-//       {/* gaming */}
-//       <Route path="/gaming" element={<MainGaming />} />
-//       <Route path="/gaming/new" element={<GamingNew />} />
-//       <Route path="/gaming/comments" element={<CommentsGaming />} />
-//       <Route path="/gaming/ask" element={<GamingAsk />} />
-//       <Route path="/gaming/show" element={<GamingShow />} />
-//       <Route path="/gaming/jobs" element={<GamingJobs />} />
-//       <Route path="/gaming/submit" element={<GamingSubmit />} />
+      <Route path="/gaming/user" element={<CommentsCrypto />} />
+      <Route path="/gaming/user/comments" element={<CommentsCrypto />} />
+      <Route path="/gaming/user/submissions" element={<CommentsCrypto />} />
+      <Route path="/gaming/user/votes" element={<CommentsCrypto />} /> */}
+    </Routes>
+  );
+}
 
-//       <Route path="/gaming/profile" element={<CommentsCrypto />} />
-//       <Route path="/gaming/profile/jobs" element={<CommentsCrypto />} />
-
-//       <Route path="/gaming/comments/selected" element={<CommentsCrypto />} />
-//       <Route path="/gaming/comments/reply" element={<CommentsCrypto />} />
-
-//       <Route path="/gaming/item" element={<CommentsCrypto />} />
-
-//       <Route path="/gaming/jobs/selected" element={<CommentsCrypto />} />
-
-//       <Route path="/gaming/user" element={<CommentsCrypto />} />
-//       <Route path="/gaming/user/comments" element={<CommentsCrypto />} />
-//       <Route path="/gaming/user/submissions" element={<CommentsCrypto />} />
-//       <Route path="/gaming/user/votes" element={<CommentsCrypto />} />
-//     </Routes>
-//   );
-// }
-
-// export default App;
+export default App;
